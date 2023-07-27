1bad7eba70792c161071ca8e08c188344329723b
Efraim Nabil <efraimefefraim@gmail.com>
2023-07-27 04:18:39 +0300

    update

--- read.c
+++ read.c
@@ -1,4 +1,8 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
 
 /**
  * read_command - Read a line of input from stdin.
@@ -9,6 +13,8 @@
 char *read_command(void)
 {
 	char buffer[BUFFER_SIZE];
+	char *command;
+
 	char *input = fgets(buffer, BUFFER_SIZE, stdin);
 
 	if (input == NULL)
@@ -22,5 +28,14 @@ char *read_command(void)
 
 	buffer[strcspn(buffer, "\n")] = '\0';
 
-	return (buffer);
+	command = malloc(strlen(buffer) + 1);
+	if (command == NULL)
+	{
+		perror("Memory allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	strcpy(command, buffer);
+
+	return (command);
 }
